a1b2b6be1d1963a80b479c1971998b59f36fc495
sethm08 <seth_mack@outlook.com>
2026-03-08 19:30:45 -0700

    Added RS50_Multicat.ipynb and accompanying dataset_builder classes/functions.

--- dataset_builder.py
+++ dataset_builder.py
@@ -9,6 +9,10 @@ from sklearn.model_selection import train_test_split
 from torch import tensor, Tensor
 from torchvision.io import read_image
 from torch.utils.data import Dataset
+import numpy as np
+import matplotlib.pyplot as plt
+from collections import Counter
+import kagglehub
 
 # 1) download
 # 2) structure (input/output as a Dataset)
@@ -40,24 +44,24 @@ def get_img_files(dir: str | Path) -> list:
     return files_list
 
 
-def idb2_image_import(path: str | Path) -> tuple[Tensor, int]:
+def image_import(path: str | Path) -> Tensor:
     """
     Desc:   Import the image and extract the blast value.
     Args:
         Path {str | Path}: Path to the image file.
     Return:
-        tuple(tensor, int)
+        Tensor
     """
+
     path = str(path)
-    blast_value, extension = path.rsplit('.', maxsplit=1)
-    blast_value = int(blast_value.rsplit('_', maxsplit=1)[1])
+    extension = path.rsplit('.', maxsplit=1)[1]
 
     if extension in ('jpg', 'jpeg', 'png'):
         img_tensor = read_image(path=path)
     else:
         img_tensor = tensor(imread(path))
 
-    return img_tensor, blast_value
+    return img_tensor
 
 
 class idb2_dataset(Dataset):
@@ -67,9 +71,24 @@ class idb2_dataset(Dataset):
 
         self.transform = transform
 
+        def label_value(path: str | Path) -> int:
+            """
+            Desc:   Extract the blast value from the file path.
+            Args:
+                path {str | Path}: Path to the image file.
+            Return:
+                int: Blast value 0 = non-blast cell, 1 = blast cell.
+            """
+
+            path = str(path)
+            blast_value = path.rsplit('.', maxsplit=1)[0]
+            blast_value = int(blast_value.rsplit('_', maxsplit=1)[1])
+
+            return blast_value
+
         # Compile a list of the image files & labels:
         img_files = get_img_files(dir=img_dir)
-        labels = [idb2_image_import(f)[1] for f in img_files]
+        labels = [label_value(f) for f in img_files]
 
         # Split the train and test files so that the labels are balanced:
         train_files, test_files = train_test_split(
@@ -79,13 +98,110 @@ class idb2_dataset(Dataset):
             stratify=labels
         )
 
+        # Set the appropriate train/test file paths and labels.
         self.img_files = train_files if train else test_files
+        self.labels = [label_value(f) for f in self.img_files]
 
     def __len__(self) -> int:
         return len(self.img_files)
 
     def __getitem__(self, idx) -> tuple:
-        image, label = idb2_image_import(self.img_files[idx])
+        image = image_import(self.img_files[idx])
+        label = self.labels[idx]
         if self.transform:
             image = self.transform(image)
         return image, label
+
+
+class bcicd_dataset(Dataset):
+    def __init__(self, transform=None, train: bool = True,
+                 split_ratio: float = 0.8, dir: str | Path = './data/.cache'):
+
+        self.transform = transform
+
+        # Download the dataset from Kaggle:
+        path = Path(
+            kagglehub.dataset_download(
+                "sumithsingh/blood-cell-images-for-cancer-detection",
+                output_dir=str(cache_path)
+                )
+        )
+
+        # File prefix to cell type dictionary:
+        prefix_dict = {
+            'BA': {'name': 'basophil', 'id': 0},
+            'ERB': {'name': 'erythroblast', 'id': 1},
+            'MO': {'name': 'monocyte', 'id': 2},
+            'MYO': {'name': 'myeloblast', 'id': 3},
+            'NGS': {'name': 'seg_neutrophil', 'id': 4},
+        }
+
+        def label_value(path: str | Path) -> int:
+            """
+            Desc:   Convert the file prefix to a label value.
+            Args:
+                path {str | Path}: Filepath.
+            Return:
+                int
+            """
+
+            filename = str(path).rsplit('\\', maxsplit=1)[1]
+            prefix = filename.split('_', maxsplit=1)[0]
+
+            return prefix_dict[prefix]['id']
+
+        img_files = get_img_files(dir=path)
+        labels = [label_value(f) for f in img_files]
+
+        # Split the train and test files so that the labels are balanced:
+        train_files, test_files = train_test_split(
+            img_files,
+            test_size=1.0 - split_ratio,
+            random_state=5,
+            stratify=labels
+        )
+
+        # Set the appropriate train/test file paths and labels.
+        self.img_files = train_files if train else test_files
+        self.labels = [label_value(f) for f in self.img_files]
+
+    def __len__(self):
+        return len(self.img_files)
+
+    def __getitem__(self, idx):
+        image = image_import(self.img_files[idx])
+        label = self.labels[idx]
+        if self.transform:
+            image = self.transform(image)
+        return image, label
+
+
+def category_balance_plot(train_dataset: Dataset, test_dataset: Dataset):
+    train_label_count = Counter(train_dataset.labels)
+    test_label_count = Counter(test_dataset.labels)
+
+    categories = sorted(set(train_label_count) | set(test_label_count))
+
+    train_percent = [
+        100 * train_label_count.get(cat, 0) / len(train_dataset)
+        for cat in categories]
+    test_percent = [
+        100 * test_label_count.get(cat, 0) / len(test_dataset)
+        for cat in categories]
+
+    label_x = np.arange(len(categories))
+    width = 0.4
+
+    plt.figure(figsize=(8,5))
+
+    plt.bar(label_x - width/2, train_percent, width, label="Train")
+    plt.bar(label_x + width/2, test_percent, width, label="Test")
+
+    plt.xticks(label_x, categories)
+    plt.ylabel("Percentage of Samples (%)")
+    plt.xlabel("Category")
+    plt.title("Train vs Test Label Distribution")
+    plt.legend()
+
+    plt.tight_layout()
+    plt.show()
